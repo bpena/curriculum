d329e99a824591ac77360b4b8a6e2d4d4c6f72d1
bpena <bernardo.pena.ramos@gmail.com>
2019-02-28 09:15:29 -0400

    adding detail route

--- src/app/modules/main/modules/curriculum/curriculum-routing.module.ts
+++ src/app/modules/main/modules/curriculum/curriculum-routing.module.ts
@@ -1,25 +1,15 @@
+import { CurriculumDetailComponent } from './views/curriculum-detail/curriculum-detail.component';
 import { CurriculumViewComponent } from './views/curriculum-view/curriculum-view.component';
 import { CurriculumEditComponent } from './views/curriculum-edit/curriculum-edit.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: CurriculumViewComponent
-    },
-    {
-        path: 'view',
-        component: CurriculumViewComponent
-    },
-    {
-        path: 'new',
-        component: CurriculumEditComponent
-    },
-    {
-        path: 'edit/:id',
-        component: CurriculumEditComponent
-    }
+    { path: '', component: CurriculumViewComponent },
+    { path: 'view', component: CurriculumViewComponent },
+    { path: 'new', component: CurriculumEditComponent },
+    { path: 'edit/:id', component: CurriculumEditComponent },
+    { path: 'detail/:id', component: CurriculumDetailComponent }
 ];
 
 @NgModule({
